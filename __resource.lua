31c649e4ec6901ffc09e0ba52b1f280bfb4160ce
Walter White <walterwhiteheisenberg84@outlook.fr>
2018-03-07 09:08:43 +0100

    Update __resource.lua

--- __resource.lua
+++ __resource.lua
@@ -4,9 +4,9 @@
 #                      #
 #     Walter White     #
 #                      #
-#     Irtas Momaki     #
 #                      #
 #         2018         #
+#                      #
 #                      #
 ########################
 
@@ -15,9 +15,13 @@
 resource_manifest_version "44febabe-d386-4d18-afbe-5e627f4af937"
 
 client_script {
+
+	'cfg/config.lua',
 	'radarfix_client.lua'
 }
 
 server_scripts {
+
+	'cfg/config.lua',
 	'radarfix_serveur.lua'
 }
